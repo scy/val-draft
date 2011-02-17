a0c87887ac3e77cf42d97c660502862f82a38f9b
Tim Weber <scy@scytale.name>
2011-02-17 18:34:20 +0100

    Let the new index.php handle file lookups

---
+++ pages/home.php
@@ -0,0 +1,18 @@
+<?php
+
+$page = 'home';
+
+$center = label('Front');
+
+$right = text('
+<h3>Val Sainte Marie</h3>
+<p>Eine Ferienkolonie im Grünen</p>
+<hr />
+<p>
+	Volorit imporis assed expeles enist voluptatur?
+	<br />
+	Turenimet pre porest, veliquosa pos si aut maionse rferuptatur? Occatesed modit es ute officatis del ilit, sunt, optas alic tota cus.
+	<br />
+	Atiur? Quibus etum qui dolorum e
+</p>
+');
